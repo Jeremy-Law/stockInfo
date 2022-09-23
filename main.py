d2d87c4188a85b3b302e6113c60e7549cc0f807e
Jeremy-Law <jeremylaw2021@gmail.com>
2022-09-23 17:14:19 -0400

    PySimpleGUI installed and working

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import googlefinance
+import googlefinance as goo
+import PySimpleGUI as sg
 
 def main():
-    print("Hello World")
+    sg.Window(title="Today's Stock information", layout=[[]], margins=(100, 50)).read()
 
 if __name__ == "__main__":
     main()
