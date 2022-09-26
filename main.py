702cdba9e26ae2a43cb19ea7feafd4ab4291028f
Jeremy-Law <jeremylaw2021@gmail.com>
2022-09-25 21:29:37 -0400

    creating csv files from downloads off of Yahoo Finance

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import googlefinance as goo
-import PySimpleGUI as sg
-
+import json
+import urllib.request
+from urllib.request import urlretrieve as urlget
+import csv
 def main():
-    sg.Window(title="Today's Stock information", layout=[[]], margins=(100, 50)).read()
+    aapl = "https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1632619273&period2=1664155273&interval=1d&events=history&includeAdjustedClose=true"
+    urlget(aapl, 'aapl.csv')
 
-if __name__ == "__main__":
+
+
+
+if __name__ == '__main__':
     main()
